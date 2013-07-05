0053589c4e667a6f7230977f448ee4f222d4e24e
Ranler Cao <findfunaax@gmail.com>
2013-07-05 22:37:52 +0800

    fix testing script

--- test/run_test.sh
+++ test/run_test.sh
@@ -7,24 +7,38 @@ ShSo_SERVER=127.0.0.1:7070
 BASE_DIR=..
 LOG_DIR=${BASE_DIR}/test/log
 
+WAIT_TIME=1
+
 
 rm -rf $LOG_DIR
 mkdir $LOG_DIR
 
 
 ### HTTP Server
+echo "start http server..."
 python2 -m SimpleHTTPServer > ${LOG_DIR}/httpserver.log 2>&1 &
+sleep ${WAIT_TIME}
 
 ### Client
+echo "start ShSo client..."
 python2 shadowsocks-client/client.py > ${LOG_DIR}/client.log 2>&1 &
+sleep ${WAIT_TIME}
 
 ### Server
-${BASE_DIR}/bin/mp_server > ${LOG_DIR}/server.log 2>&1 &
+echo "start ShSo server"
+LD_LIBRARY_PATH=${BASE_DIR}/lib ${BASE_DIR}/bin/mp_server > ${LOG_DIR}/server.log 2>&1 &
+sleep ${WAIT_TIME}
 
 
 ### testing commands
+echo "start testing"
+{
+    for i in {1..100};do
+	curl http://${HTTP_SERVER}/ --socks5 ${ShSo_CLIENT} &
+    done
+    wait
+} | tee ${LOG_DIR}/testing.log
 
-curl http://${HTTP_SERVER}/ --socks5 ${ShSo_CLIENT}
 
-
-## trap "exit 0" 15
+## kill all subprocess
+kill -15 -- `jobs -p`
