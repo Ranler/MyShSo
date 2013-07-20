51c2909958164442a7a67794740edd36584e2cd7
RanlerCao <findfunaax@gmail.com>
2013-07-20 10:58:26 +0800

    fix syn errors

--- src/muduo_server/ShSoServer.cpp
+++ src/muduo_server/ShSoServer.cpp
@@ -1,8 +1,6 @@
 #include <map>
-#include <boost/bind>
-#include "muduo/net/TcpServer.h"
+#include <boost/bind/bind.hpp>
 #include "ShSoServer.h"
-
 
 using namespace boost;
 using namespace muduo;
@@ -12,8 +10,8 @@ ShSoServer::ShSoServer(EventLoop *loop, const InetAddress &listenAddr)
   : _loop(loop),
     _server(loop, listenAddr, "ShSoServer")
 {
-  _server.setConnectionCallback(bind(&ShSoServer::onConnection, this, _1));
-  _server.setMessageCallback(bind(&ShSoServer::onMessage, this, _1, _2, _3));
+  _server.setConnectionCallback(bind(&ShSoServer::onServerConnection, this, _1));
+  _server.setMessageCallback(bind(&ShSoServer::onServerMessage, this, _1, _2, _3));
 }
 
 
@@ -30,12 +28,48 @@ void ShSoServer::onServerConnection(const TcpConnectionPtr& conn)
   }
 }
 
+static shared_ptr<InetAddress> getAddr(Buffer* buf)
+{
+  int8_t addrtype = buf->peekInt8();
+  
+  shared_ptr<InetAddress> paddr;
+  switch(addrtype) {
+  case ShSoServer::ADDRTYPE_IPV4:    /* |addr(4)|+|port(2)|*/
+    {
+      if (buf->readableBytes() < 7) break;   // wait to next callback
 
+      sockaddr_in addr;
+      const void* ip = buf->peek() + 1;
+      const void* port = buf->peek() + 5;
+      bzero(&addr, sizeof addr);
+      addr.sin_family = AF_INET;
+      addr.sin_port = *static_cast<const in_port_t*>(port);
+      addr.sin_addr = *static_cast<const in_addr*>(ip);
+      
+      paddr = shared_ptr<InetAddress>(new InetAddress(addr));
+    }
+      break;
+    
+  case ShSoServer::ADDRTYPE_DOMAIN:    /* |addr_len(1)|+|host(addr_len)|+|port(2)| */
+      break;
+    
+  case ShSoServer::ADDRTYPE_IPV6:  /* |addr(16)|+|port(2)|*/      
+      break;
+      
+  }
+  
+  return paddr;
+}
 
 void ShSoServer::onServerMessage(const TcpConnectionPtr& conn, Buffer* buf, Timestamp time)
 {
-  if (_tunnels.find(conn->name()) == _tunnels.end()) {    // tunnel not create
-    
+  if (_tunnels.find(conn->name()) == _tunnels.end()) {    // tunnel not connected
+    shared_ptr<InetAddress> serverAddr = getAddr(buf);
+    if (serverAddr) {
+      TunnelPtr tunnel(new Tunnel(_loop, *serverAddr, conn));
+      tunnel->start();
+      _tunnels[conn->name()] = tunnel;
+    }
   } else {
      const TcpConnectionPtr &clientConn
        = boost::any_cast<const TcpConnectionPtr&>(conn->getContext());
--- src/muduo_server/ShSoServer.h
+++ src/muduo_server/ShSoServer.h
@@ -1,6 +1,8 @@
 #ifndef _MY_SHSOSERVER_
 #define _MY_SHSOSERVER_
 
+#include <string>
+#include <map>
 #include "Tunnel.h"
 #include "muduo/net/TcpServer.h"
 
@@ -9,6 +11,11 @@ class ShSoServer
  public:
   ShSoServer(muduo::net::EventLoop *loop, 
 	     const muduo::net::InetAddress &listenAddr);
+
+  const static int ADDRTYPE_IPV4 = 1;
+  const static int ADDRTYPE_DOMAIN = 3;
+  const static int ADDRTYPE_IPV6 = 4;
+  
  private:
   // callback functions
   void onServerConnection(const muduo::net::TcpConnectionPtr& conn);
@@ -20,7 +27,7 @@ class ShSoServer
   muduo::net::EventLoop *_loop;
   muduo::net::TcpServer _server;
 
-  std::map<string, TunnelPtr> _tunnels;
+  std::map<muduo::string, TunnelPtr> _tunnels;
 };
 
 #endif
